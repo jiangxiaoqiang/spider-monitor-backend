dd0912ed31fa2b1e8f2daf3c3fe0d474f0b9907c
jiangxiaoqiang <jiangtingqiang@gmail.com>
2019-02-21 23:47:47 +0800

    repair datetime format

--- monitor/data/src/main/java/model/SpiderAnalysis.java
+++ monitor/data/src/main/java/model/SpiderAnalysis.java
@@ -1,5 +1,6 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,5 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class SpiderAnalysis {
     private Integer bookTotalElements;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime analysisTimestamp;
 }
